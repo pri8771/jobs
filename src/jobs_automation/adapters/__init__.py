a4348a562dc4b6659e3083ec584ba9427ec5443f
pri8771 <13511421+pri8771@users.noreply.github.com>
2026-09-20 22:40:28 -0400

    Export deterministic model gateway

--- src/jobs_automation/adapters/__init__.py
+++ src/jobs_automation/adapters/__init__.py
@@ -2,10 +2,11 @@
 
 from jobs_automation.adapters.base import ATSAdapter, EmailAdapter, ModelGateway
 from jobs_automation.adapters.gmail import GmailAdapter, GmailOAuthClient, MockEmailAdapter
-from jobs_automation.adapters.models import LiteLLMModelGateway, MockModelGateway
+from jobs_automation.adapters.models import DeterministicModelGateway, LiteLLMModelGateway, MockModelGateway
 
 __all__ = [
     "ATSAdapter",
+    "DeterministicModelGateway",
     "EmailAdapter",
     "GmailAdapter",
     "GmailOAuthClient",
